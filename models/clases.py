40ba7c397dac9f8b14a2d3f3335f82c640d0e4a1
sogramas <sofiagracia000@gmail.com>
2025-01-10 10:44:45 +0100

    Afegim date per defecte a eventos.py

--- models/clases.py
+++ models/clases.py
@@ -15,6 +15,7 @@ class clases_escola(models.Model):
     alumnos = fields.One2many(comodel_name='escola.alumnos', inverse_name='clase',string="Alumnos de la clase")
     total_alumnos = fields.Integer(string="Total de alumnos",compute="_compute_alumnos")
     descripcion = fields.Char(string="Descripción", required=True, help="Introduce una descripción")
+    activo = fields.Boolean(string="Activo", default=True)
     
     @api.depends('alumnos')
     def _compute_alumnos(self):
--- models/eventos.py
+++ models/eventos.py
@@ -1,14 +1,22 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import datetime
 
 class eventos_escola(models.Model):
     _name = 'escola.eventos'
     _description = 'escola.eventos'
 
+    #Hem de posar-li un nom a l'event
+    nombre = fields.Char(string="Nombre del evento", compute="_compute_nom")
+
+    # Esta funció tamb pot ser una lambda
+    #def _obtindre_data(self):
+    #    return datetime.datetime.now()
 
     tipo = fields.Selection([('0','Ausencia'), ( '1','Retraso'), ('2','Felicitación'), ('3','Comportamiento')], string="Seleccion", default="0")
-    fecha = fields.Date(string="Data d'Inici")
+    # Si volguerem mostrar també l'hora deuria de ser de tipus Datetime
+    fecha = fields.Date(string="Data d'Inici",default=lambda d: datetime.datetime.now())
     descripcion = fields.Char(string="Descripción", required=True, help="Introduce la descripción del evento")
     
     
@@ -19,3 +27,8 @@ class eventos_escola(models.Model):
     # profesor = fields.Many2many('escola.profesors', string="Professors")
     # Relación con alumno
     alumno = fields.Many2many('escola.alumnos', string="Alumnos")
+    
+    @api.depends('clase')
+    def _compute_nom(self):
+        for r in self:
+            
